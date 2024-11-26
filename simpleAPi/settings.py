743d3b0554d3b7fd54e07811305798e3a3bb1081
azamtoiri <azamtoiri@gmail.com>
2024-11-26 14:44:56 +0300

    add env varibles and cors disabled

--- simpleAPi/settings.py
+++ simpleAPi/settings.py
@@ -36,9 +36,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'simpleAPi.urls'
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',  # ваш Svelte-приложение
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',  # ваш Svelte-приложение
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -58,9 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'simpleAPi.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -68,9 +66,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,9 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -100,13 +92,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
